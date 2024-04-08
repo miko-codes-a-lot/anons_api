0993d2ba91c618b7acb1cb57143afe0bae70a322
Miko Chu <miko.codecrafters@gmail.com>
2024-04-09 00:26:15 +0800

    added api/secure/register http post rest api

--- src/main/java/com/anons/api/controllers/SecureController.java
+++ src/main/java/com/anons/api/controllers/SecureController.java
@@ -1,6 +1,8 @@
 package com.anons.api.controllers;
 
+import com.anons.api.models.dto.UserDTO;
 import com.anons.api.models.form.UserLoginForm;
+import com.anons.api.models.form.UserRegisterForm;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,5 +14,16 @@ public class SecureController {
     public String login(@RequestBody UserLoginForm user) {
         System.out.println("User: " + user);
         return "JWT";
+    }
+
+    @PostMapping("/register")
+    @ResponseStatus(HttpStatus.OK)
+    public UserDTO register(@RequestBody UserRegisterForm user) {
+        return UserDTO.builder()
+                .id(1L)
+                .username(user.getUsername())
+                .email(user.getEmail())
+                .phoneNumber(user.getPhoneNumber())
+                .build();
     }
 }
